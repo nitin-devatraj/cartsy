9527e1a25b574db69d6555c8229866245696922d
nitin devatraj <nitindevatraj@protonmail.com>
2023-07-15 01:25:39 +0530

    add cartSlice & add logic to use that state slice

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,47 @@
 import ProductItem from "./ProductItem";
 import classes from "./Products.module.css";
 
+const DUMMY_PRODUCTS = [
+  {
+    title: "Galactic Goggles",
+    price: 199.99,
+    id: 1,
+    description: "Explore the universe with augmented reality goggles.",
+  },
+  {
+    title: "Time Travel Watch",
+    price: 499.99,
+    id: 2,
+    description: "Unlock the secrets of time with this revolutionary watch.",
+  },
+  {
+    title: "Infinity Energy Drink",
+    price: 4.99,
+    id: 3,
+    description: "Experience boundless energy with our cosmic drink.",
+  },
+  {
+    title: "Dreamscape Bed Sheets",
+    price: 79.99,
+    id: 4,
+    description: "Drift into fantastical dreams with our ethereal bed sheets.",
+  },
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Products</h2>
       <ul>
-        <ProductItem
-          title="Test"
-          price={6}
-          description="This is a first product - amazing!"
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            id={product.id}
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
